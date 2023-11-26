78044af377054fbfe2083bb0c0642be6049bf658
Sergey Rombak <rombak.a87@mail.ru>
2023-11-26 21:07:44 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача3
+/* Напишите программу, которая принимает на вход целое число
+из отрезка [10, 99] и показывает наибольшую цифру числа.*/
+
+Console.Clear();
+Console.Write("Введите число от 10 до 99: ");
+int number = int.Parse(Console.ReadLine()!);
+while (number < 10 || number > 99)
+{
+    Console.Write("Ошибка!\nВведите число от 10 до 99: ");
+    number = int.Parse(Console.ReadLine()!);
+}
+int firstNumber = number / 10;
+int secondNumber = number % 10;
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("Наибольшая цифра: " + firstNumber);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра: " + secondNumber);
+}
