fab8e7f2bd5326b96371fbc3cbc8002d61bb41d4
Sergey Rombak <rombak.a87@mail.ru>
2023-11-26 21:49:36 +0500

    add Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,12 +5,10 @@
  в которой находится эта точка.
 */
 
-Console.Clear();
-Console.Write("Введите координату точки X: ");
-int coordinateX = int.Parse(Console.ReadLine()!);
-Console.Write("Введите координату точки Y: ");
-int coordinateY = int.Parse(Console.ReadLine()!);
-if (coordinateX != 0 $$ coordinateY != 0)
-    Console.WriteLine("да");
-else
-    Console.WriteLine($"Ошибка, ({coordinateX = 0 $$ coordinateY = 0})");
+// Console.Clear();
+// Console.Write("Введите координату точки X: ");
+// int coordinateX = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату точки Y: ");
+// int coordinateY = int.Parse(Console.ReadLine()!);
+
+
