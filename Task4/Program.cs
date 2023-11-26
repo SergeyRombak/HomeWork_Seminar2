7c1ec1af8604b79e93f56a7f2cf0ca8de95e1716
Sergey Rombak <rombak.a87@mail.ru>
2023-11-26 21:23:12 +0500

    add Task4 не решил

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача4
+/* Напишите программу, которая на вход
+ принимает натуральное число N, 
+ а на выходе показывает его цифры через запятую.*/
+
+Console.Clear();
+Console.Write("Введите любое натуральное число: ");
+int number = int.Parse(Console.ReadLine()!);
+number = Math.Abs(number);
+int i = 0;
+for (int i = 0; i < length; i++)
+{
+    
+}
