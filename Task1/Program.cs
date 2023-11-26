4c1fa8c7ba635b3c8c3bac74a52aae4ea4c68d06
Sergey Rombak <rombak.a87@mail.ru>
2023-11-26 20:03:47 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+ if (Number % 7 == 0 || Number % 23 ==0)
+     Console.WriteLine("да");
+ else
+     Console.WriteLine($"нет, {Number % 7 == 0 || Number % 23 ==0}");
+
