4c879749a411837e2e76c3ede81c1fdeccc374f8
Sergey Rombak <rombak.a87@mail.ru>
2023-11-28 20:49:00 +0500

    add Task4 не решил

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,9 +6,5 @@
 Console.Clear();
 Console.Write("Введите любое натуральное число: ");
 int number = int.Parse(Console.ReadLine()!);
-number = Math.Abs(number);
-int i = 0;
-for (int i = 0; i < length; i++)
-{
-    
-}
+
+//Не понял как решить!!!
